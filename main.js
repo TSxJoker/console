d1c65ec20aeb70acfca2575f706d62f38a627836
TSxJoker <81158707+TSxJoker@users.noreply.github.com>
2021-09-06 17:03:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("My first console project");
+ var first_name = "Kanav"; 
+ var last_name = "Sharma"; 
+ var full_name = first_name.concat(last_name);
+  console.log(full_name);
+   var a = 20; 
+   var b = 10; 
+   var add = a + b ; 
+   console.log(add); 
+   var sub = a - b ; 
+   console.log(sub);
+    var multiple = a * b ;
+     console.log(multiple);
+      var divide = a / b ; console.log(divide);
+      
